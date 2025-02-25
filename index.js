806ce7b8f71dda9180f59f6acc4d76bf77a5b7e2
Gourav-RM <mishragourav325@gmail.com>
2025-02-25 16:02:54 +0530

    test route added

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ import express from "express";
 const app = express();
 
 app.listen(8081, () => console.log("Server is started............"))
+
+app.get('/', (req, res) => {
+    res.status(200).send('Application is running!!!')
+})
