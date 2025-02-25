0ed98f99844861c5c45242754af2178b46da73b6
Gourav-RM <mishragourav325@gmail.com>
2025-02-26 02:40:25 +0530

    feat:response changed

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ app.listen(8081, () => console.log("Server is started............"))
 
 app.get('/', (req, res) => {
     console.log("request received: ", new Date())
-    res.status(200).json({statusCode: 200, data: 'Server is up!!!'})
+    res.status(200).json({statusCode: 200, data: 'Server is up and running!!!'})
 })
 
