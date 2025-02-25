b2c18c8ffbb7db8e0132c9bbbc4bbfea0e28f140
Gourav-RM <mishragourav325@gmail.com>
2025-02-25 18:43:24 +0530

    log added

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const app = express();
 app.listen(8081, () => console.log("Server is started............"))
 
 app.get('/', (req, res) => {
+    console.log("request received: ", new Date())
     res.status(200).send('Application is running!!')
 })
